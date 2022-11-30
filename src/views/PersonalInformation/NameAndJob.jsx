2f83b54e8a6e734024fc16d33208e3cbcbb65017
mojleon <damirkalbic@gmail.com>
2022-11-30 19:23:35 +0100

    Added profile Textarea.

--- src/views/PersonalInformation/NameAndJob.jsx
+++ src/views/PersonalInformation/NameAndJob.jsx
@@ -1,27 +1,33 @@
 import React, { Component } from "react";
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
 
 function NameAndJob(props) {
-    return (
-      <div id="name-and-job">
-        <h3>Name and job</h3>
-        <Input
-          placeholder="First name"
-          name="firstname"
-          onStateChange={props.onStateChange}
-        />
-        <Input
-          placeholder="Last name"
-          name="lastname"
-          onStateChange={props.onStateChange}
-        />
-        <Input
-          placeholder="Title"
-          name="title"
-          onStateChange={props.onStateChange}
-        />
-      </div>
-    );
+  return (
+    <div id="name-and-job">
+      <h3>Name and job</h3>
+      <Input
+        placeholder="First name"
+        name="firstname"
+        onStateChange={props.onStateChange}
+      />
+      <Input
+        placeholder="Last name"
+        name="lastname"
+        onStateChange={props.onStateChange}
+      />
+      <Input
+        placeholder="Title"
+        name="title"
+        onStateChange={props.onStateChange}
+      />
+      <Textarea
+        placeholder="Profile"
+        name="profile"
+        onStateChange={props.onStateChange}
+      />
+    </div>
+  );
 }
 
 export default NameAndJob;
